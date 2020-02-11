70fcda1d753b70be41a14a1e0185bfaf14ebbfa8
CristianUser <cristianmejia97@gmail.com>
2020-02-10 23:12:57 -0400

    eslint and prettier set

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,20 +3,24 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint'
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'eol-last': [2, 'always'],
+    'max-params': [2, 4],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-floating-decimal': 1,
+    'no-var': 2,
+    quotes: ['error', 'single'],
+    'prefer-const': 2,
+    'prefer-template': 2
   },
   overrides: [
     {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
-      ],
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
       env: {
         jest: true
       }
